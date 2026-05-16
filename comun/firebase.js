a94c997fa651dc7dcff9bac574dd3475e3b8db57
Iratxe <lasa.133343@e.unavarra.es>
2026-05-16 13:07:35 +0200

    Archivo de configuración de Firebase y dependencias

---
+++ comun/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBRWP1eMTlP5pFR2pOL1eN1XOAc4z8bGqQ",
+    authDomain: "sanferlink-5a65c.firebaseapp.com",
+    projectId: "sanferlink-5a65c",
+    storageBucket: "sanferlink-5a65c.firebasestorage.app",
+    messagingSenderId: "539998531895",
+    appId: "1:539998531895:web:7ff1140c0fc273f55deab2",
+    databaseURL: "https://sanferlink-5a65c-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);   //Para las alertas/incidencias
+export const rtdb = getDatabase(app); //Para el estado "connected"
