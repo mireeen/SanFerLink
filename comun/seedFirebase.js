f3ff6784f2e09ac87d21ac3069bea7bce0ea1057
mireeen <mailharin.148443@e.unavarra.es>
2026-06-08 14:58:13 +0200

    Ultimos retoques

--- comun/seedFirebase.js
+++ comun/seedFirebase.js
@@ -312,70 +312,62 @@ const baños = {
   }
 };
 
-const getCurrentTimestamp = () => new Date().toISOString();
+const getCurrentTimestamp = () => new Date().getTime();
 
 const alertas = {
-  user_test_1: {
-    tipo: "Calle colapsada",
-    descripcion: "Estafeta recién colapsada durante el encierro. Muchísima densidad de gente.",
-    latitud: 42.8180,
-    longitud: -1.6433,
-    timestamp: getCurrentTimestamp(),
-    userId: "user_test_1"
-  },
+  
 
-  user_test_2: {
-    tipo: "Calle colapsada",
-    descripcion: "Mercaderes con flujo lento de personas, pero transitable.",
-    latitud: 42.8183,
-    longitud: -1.6444,
-    timestamp: getCurrentTimestamp(),
-    userId: "user_test_2"
-  },
+  
 
   user_test_3: {
+    calle: "Paseo Sarasate",
     tipo: "Calle colapsada",
-    descripcion: "Santo Domingo con acumulación antigua, la situación parece mejorar.",
-    latitud: 42.8189,
-    longitud: -1.6456,
+    descripcion: "Paseo Sarasate completamente lleno, especialmente en la zona de la fuente. Evitar si se puede.",
+    latitud: 42.81542219654759,
+    longitud: -1.6450727185001823,
     timestamp: getCurrentTimestamp(),
     userId: "user_test_3"
   },
 
   user_test_4: {
+    calle: "Calle Jarauta",
     tipo: "Calle colapsada",
     descripcion: "Jarauta completamente llena en zona de bares.",
-    latitud: 42.8195,
-    longitud: -1.6462,
+    latitud: 42.81835264007162, 
+    longitud: -1.6480246582813716,
     timestamp: getCurrentTimestamp(),
     userId: "user_test_4"
   },
 
   user_test_5: {
+    calle: "Calle Navarrería",
     tipo: "Calle colapsada",
-    descripcion: "Zona de Taconera con bastante afluencia, pero en movimiento.",
-    latitud: 42.8197,
-    longitud: -1.6510,
+    descripcion: "Ha habido algún problema y está cerrada.",
+    latitud: 42.81956264823931, 
+    longitud: -1.642269716561121,
     timestamp: getCurrentTimestamp(),
     userId: "user_test_5"
   },
 
   user_test_6: {
+    calle: "Calle Calderería",
     tipo: "Calle colapsada",
-    descripcion: "Plaza del Castillo con afluencia anterior ya reduciéndose.",
-    latitud: 42.8168,
-    longitud: -1.6435,
-    timestamp: getCurrentTimestamp(),
+    descripcion: "Calle caldera imposible.",
+    latitud: 42.81815837630921,
+    longitud:  -1.6418293915409223,
+    timestamp: getCurrentTimestamp()-5*60*1000,
     userId: "user_test_6"
   },
 
-  OuNIE3Jpf1avqi6lOf: {
+  user_test_7: {
+    calle: "Calle mayor",
     tipo: "Calle colapsada",
-    descripcion: "gidks",
-    latitud: 42.7993819,
-    longitud: -1.6355467,
-    timestamp: getCurrentTimestamp(),
-    userId: "1nssvfu9W8SOsTU5R7FYNKKPsfJ3"
+    descripcion: "Calle Mayor super llena",
+    latitud: 42.81770442060797,
+    longitud: -1.6474228423649075,
+    timestamp: getCurrentTimestamp()-4*60*1000,
+    userId: "user_test_7"
+    
   }
 };
 
@@ -411,22 +403,22 @@ const zonasInteres = {
 
 async function seed() {
   try {
-    console.log("📦 Subiendo eventos...");
-    await set(ref(rtdb, "zonasInteres"), zonasInteres);
+    // console.log("📦 Subiendo eventos...");
+    // await set(ref(rtdb, "zonasInteres"), zonasInteres);
 
     //console.log("🚽 Subiendo baños...");
     //await set(ref(rtdb, "baños"), baños);
 
-    // console.log("🚨 Subiendo alertas...");
-    // const alertasRef = ref(rtdb, "alertas");
+    console.log("🚨 Subiendo alertas...");
+    const alertasRef = ref(rtdb, "alertas");
 
-    // // Primero intenta leer si existe
-    // const snapshot = await get(alertasRef);
-    // console.log("¿Existe alertas?", snapshot.exists());
+    // Primero intenta leer si existe
+    const snapshot = await get(alertasRef);
+    console.log("¿Existe alertas?", snapshot.exists());
 
-    // Luego sube los datos (si no existe, Firebase lo crea)
-    // await set(alertasRef, alertas);
-    // console.log("✅ Alertas subidas correctamente");
+    // Luego sube los datos (}si no existe, Firebase lo crea)
+    await set(alertasRef, alertas);
+    console.log("✅ Alertas subidas correctamente");
 
     console.log("✅ Seed completado correctamente");
   } catch (err) {
